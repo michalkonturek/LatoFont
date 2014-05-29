d7a4e858a54a9ea2a66e420cc77fe9ec88b85b95
Michal Konturek <michal.konturek@gmail.com>
2014-05-29 14:28:58 +0100

    Done ExampleVC.

--- LatoFont/LatoFont/ExampleVC.m
+++ LatoFont/LatoFont/ExampleVC.m
@@ -8,14 +8,29 @@
 
 #import "ExampleVC.h"
 
-@interface ExampleVC ()
-
-@end
+#import "UIFont+Lato.h"
 
 @implementation ExampleVC
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    CGFloat size = 17.0;
+    
+    self.hairlineLabel.font = [UIFont latoHairlineFontOfSize:size];
+    self.hairlineItalicLabel.font = [UIFont latoHairlineItalicFontOfSize:size];
+    
+    self.lightLabel.font = [UIFont latoLightFontOfSize:size];
+    self.lightItalicLabel.font = [UIFont latoLightItalicFontOfSize:size];
+    
+    self.regularLabel.font = [UIFont latoFontOfSize:size];
+    self.italicLabel.font = [UIFont latoItalicFontOfSize:size];
+    
+    self.boldLabel.font = [UIFont latoBoldFontOfSize:size];
+    self.boldItalicLabel.font = [UIFont latoBoldItalicFontOfSize:size];
+    
+    self.blackLabel.font = [UIFont latoBlackFontOfSize:size];
+    self.blackItalicLabel.font = [UIFont latoBlackItalicFontOfSize:size];
 }
 
 - (void)didReceiveMemoryWarning {
