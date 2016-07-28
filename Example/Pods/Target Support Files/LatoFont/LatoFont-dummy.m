26f1ef81dc02859a2037156ec63cab536d5e8f18
Michal Konturek <michal.konturek@gmail.com>
2016-07-28 23:32:51 +0100

    Remvoed Pod directory

--- Example/Pods/Target Support Files/LatoFont/LatoFont-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LatoFont : NSObject
-@end
-@implementation PodsDummy_LatoFont
-@end
--- Example/Pods/Target Support Files/LatoFont/LatoFont-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double LatoFontVersionNumber;
-FOUNDATION_EXPORT const unsigned char LatoFontVersionString[];
-
